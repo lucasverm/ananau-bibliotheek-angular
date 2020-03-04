aa77e3ae0bfaf62131748e1a46bbfcaed2952241
lucasverm <lucasvermeulen@gmail.com>
2020-03-04 17:50:26 -0500

    fixing topbar with language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { LanguageComponent } from './language/language.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TopbarComponent
+    TopbarComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
