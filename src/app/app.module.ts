b89c4a46a117fbd211cdd3c24e04f1db4bb0aa0a
lucasverm <lucasvermeulen@gmail.com>
2020-03-04 13:32:42 -0500

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LanguageComponent } from './language/language.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
