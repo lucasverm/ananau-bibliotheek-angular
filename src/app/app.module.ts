e9b6fab0e721be4d98231bbdc3838e94f69edc3f
lucasverm <lucasvermeulen@gmail.com>
2020-03-04 15:24:17 -0500

    add menubalk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
