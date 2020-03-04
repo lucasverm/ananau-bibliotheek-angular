cf069664f41b4ff7f20f361e594c6ca5a7c49fea
lucasverm <lucasvermeulen@gmail.com>
2020-03-04 17:33:25 -0500

    Login pagina werkende met error handeling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TopbarComponent } from './topbar/topbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginFormulier: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.loginFormulier = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
   }
 
+  logGebruikerIn() {
+    alert(this.loginFormulier.value.email + '' + this.loginFormulier.value.password);
+  }
+
+  getErrorMessage(errors: any, veldNaam: string): string {
+    if (errors == null) {
+      return;
+    
+    } else if (errors.required) {
+      return veldNaam + ' is verplicht';
+    
+    } else if (errors.minlength) {
+      return `needs at least ${errors.minlength.requiredLength} 
+      characters (got ${errors.minlength.actualLength})`;
+    
+    } else if (errors.email) {
+      return `Dit is een ongeldig emailadres!`;
+    }
+  }
 }
