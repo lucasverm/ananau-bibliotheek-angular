ddb726b76c66aa9a934cabe30fcec6b8157645c2
lucasverm <lucasvermeulen@gmail.com>
2020-03-18 22:36:00 -0500

    test scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ItemPageComponent } from './item-page/item-page.component';
 import { GebruikerItemsTabelComponent } from './gebruiker-items-tabel/gebruiker-items-tabel.component';
 import { ScanItemComponent } from './scan-item/scan-item.component';
 import { QRCodeModule } from 'angular2-qrcode';
+import { NgQrScannerModule } from 'angular2-qrscanner';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { QRCodeModule } from 'angular2-qrcode';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    QRCodeModule
+    QRCodeModule,
+    NgQrScannerModule
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
