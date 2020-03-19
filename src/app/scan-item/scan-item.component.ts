daf1532c156045c9d698e0d133e4392c5d4187b7
lucasverm <lucasvermeulen@gmail.com>
2020-03-18 22:44:44 -0500

    remove test scan

--- src/app/scan-item/scan-item.component.ts
+++ src/app/scan-item/scan-item.component.ts
@@ -9,16 +9,6 @@ import { Subject } from 'rxjs';
 export class ScanItemComponent implements OnInit {
 
   public loading: Boolean;
-  chosenCameraSubject = new Subject();
-
-  decodedOutput($event: string) {
-    console.log('Decoded', $event);
-  }
-
-  listCameras($event: MediaDeviceInfo[]) {
-    console.log('MediaDeviceInfo', $event);
-    this.chosenCameraSubject.next($event.filter(device => device.kind === 'videoinput')[0])
-  }
   
   constructor() { }
 
