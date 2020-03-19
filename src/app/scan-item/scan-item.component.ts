e5e66e226387a783e6e35523158d28bc8aa2836d
lucasverm <lucasvermeulen@gmail.com>
2020-03-18 22:54:32 -0500

    add test scanner

--- src/app/scan-item/scan-item.component.ts
+++ src/app/scan-item/scan-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-scan-item',
@@ -9,7 +10,16 @@ import { Subject } from 'rxjs';
 export class ScanItemComponent implements OnInit {
 
   public loading: Boolean;
-  
+  qrResultString: string;
+
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
   constructor() { }
 
   ngOnInit() {
